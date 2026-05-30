7ade32881f78391de8f8bfb1de52dc073cf2d53a
Tradecom <trctradecom@gmail.com>
2026-05-30 11:01:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ let epochStartFromContract = 0;
 
 
 // ========================== CONTRACT ADDRESSES ==========================
-const contractAddress = "0x8A631D4104dC9ec5904FE9142bFfAD9Db2678947";
+const contractAddress = "0x419a59b26fF3FCF95cEDa75F6a695b20f7EC492E";
 const tokenAddress = "0xCcC20c59bA3827f6E40B7Debc60a804836c55740";
 
 // ========================== ABI ==========================
